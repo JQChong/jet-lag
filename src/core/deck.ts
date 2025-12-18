be4837a8c36b6b4b4aa99b0b6d3cb77f53310e66
JQChong <jingquan0302@gmail.com>
2025-12-19 02:09:41 +0800

    micro version

--- src/core/deck.ts
+++ src/core/deck.ts
@@ -45,19 +45,19 @@ export function deserializeDeck(data: DeckCardData[]): DeckCard[] {
 export const cardDefinitions: Card[] = [
 	{
 		file: 'time_bonus_5',
-		count: 25,
+		count: 20, //originally 25
 		hasMorePages: false,
 		canUse: (hand) => false,
 	},
 	{
 		file: 'time_bonus_10',
-		count: 15,
+		count: 12, //originally 15
 		hasMorePages: false,
 		canUse: (hand) => false,
 	},
 	{
 		file: 'time_bonus_15',
-		count: 10,
+		count: 6, //originally 10
 		hasMorePages: false,
 		canUse: (hand) => false,
 	},
@@ -121,7 +121,7 @@ export const cardDefinitions: Card[] = [
 		hasMorePages: false,
 		canUse: (hand) => true,
 	},
-	{
+	/*{
 		file: 'curse_bridge_troll',
 		count: 1,
 		hasMorePages: false,
@@ -129,7 +129,7 @@ export const cardDefinitions: Card[] = [
 			'To use this card, you must be (S 1) (M 10) (L 50) km away from the seekers.',
 		canUse: (hand) => true,
 	},
-	/*{
+	{
 		file: 'curse_cairn',
 		count: 1,
 		hasMorePages: false,
@@ -137,10 +137,10 @@ export const cardDefinitions: Card[] = [
 		canUse: (hand) => true,
 	},*/
 	{
-		file: 'curse_distant_cuisine',
+		file: 'curse_distant_cuisine_v2',
 		count: 1,
 		hasMorePages: false,
-		useText: 'To use this card, you must be at the restaurant.',
+		useText: 'To use this card, you must be at the stall.',
 		canUse: (hand) => true,
 	},
 	{
@@ -150,7 +150,7 @@ export const cardDefinitions: Card[] = [
 		useText: 'To use this card, you must discard your hand.',
 		canUse: (hand) => true,
 	},
-	{
+	/*{
 		file: 'curse_egg_partner',
 		count: 1,
 		hasMorePages: true,
@@ -159,7 +159,7 @@ export const cardDefinitions: Card[] = [
 			'You cannot use this card as you do not have 2 cards in your hand.',
 		canUse: (hand) => hand.length > 2,
 	},
-	/*{
+	{
 		file: 'curse_endless_tumble',
 		count: 1,
 		hasMorePages: false,
@@ -184,14 +184,14 @@ export const cardDefinitions: Card[] = [
 			'You cannot use this card as you do not have 2 cards in your hand.',
 		canUse: (hand) => hand.length > 2,
 	},
-	{
+	/*{
 		file: 'curse_impressionable_consumer',
 		count: 1,
 		hasMorePages: false,
 		useText:
 			"If you use this card, the seekers' next question is free. (This is handled by the game, be sure to log the next question.)",
 		canUse: (hand) => true,
-	},
+	},*/
 	{
 		file: 'curse_jammed_door',
 		count: 1,
@@ -208,7 +208,7 @@ export const cardDefinitions: Card[] = [
 		useText: 'To use this card, you must draw a maze.',
 		canUse: (hand) => true,
 	},
-	{
+	/*{
 		file: 'curse_lemon_phylactery',
 		count: 1,
 		hasMorePages: true,
@@ -219,7 +219,7 @@ export const cardDefinitions: Card[] = [
 			for (let card of hand) if (card.file.includes('powerup_')) return true;
 			return false;
 		},
-	},
+	},*/
 	{
 		file: 'curse_luxury_car',
 		count: 1,
@@ -244,14 +244,14 @@ export const cardDefinitions: Card[] = [
 			'You cannot use this card as you do not have a card in your hand.',
 		canUse: (hand) => hand.length > 1,
 	},
-	{
+	/*{
 		file: 'curse_ransom_note',
 		count: 1,
 		hasMorePages: false,
 		useText:
 			'To use this card, you must spell out "ransom note" as a ransom note.',
 		canUse: (hand) => true,
-	},
+	},*/
 	{
 		file: 'curse_right_turn',
 		count: 1,
@@ -287,7 +287,7 @@ export const cardDefinitions: Card[] = [
 		useText: 'To use this card, the seekers must be outside.',
 		canUse: (hand) => true,
 	},
-	{
+	/*{
 		file: 'curse_the_uturn',
 		count: 1,
 		hasMorePages: false,
@@ -310,7 +310,7 @@ export const cardDefinitions: Card[] = [
 		useText:
 			'To use this card, the seekers must be within 1,000 feet of a body of water.',
 		canUse: (hand) => true,
-	},
+	},*/
 	{
 		file: 'curse_zoologist',
 		count: 1,
@@ -319,7 +319,7 @@ export const cardDefinitions: Card[] = [
 			'To use this card, you must send the seekers a photo of an animal.',
 		canUse: (hand) => true,
 	},
-	{
+	/*{
 		file: 'curse_durian',
 		count: 1,
 		hasMorePages: false,
@@ -339,15 +339,15 @@ export const cardDefinitions: Card[] = [
 		hasMorePages: false,
 		useText: "To use this card, you must roll a die. If it's an even number, this card has no effect.",
 		canUse: (hand) => true,
-	},
+	},*/
 	{
-		file: 'curse_imaginary_sinkhole',
+		file: 'curse_imaginary_sinkhole_micro',
 		count: 1,
 		hasMorePages: false,
-		useText: "To use this card, seekers must be (S 2) (M 5) (L 10) km away from the boundary of the sinkhole.",
+		useText: "To use this card, seekers must be 250m away from the boundary of the sinkhole.",
 		canUse: (hand) => true,
 	},
-	/*{
+	{
 		file: 'curse_sloth',
 		count: 1,
 		hasMorePages: true,
@@ -358,7 +358,7 @@ export const cardDefinitions: Card[] = [
 			for (let card of hand) if (card.file.includes('time_bonus')) return true;
 			return false;
 		},
-	}*/
+	}
 ];
 
 function generateDeck(): DeckCard[] {
